7b415e72fae37709009f4b93a4866fc3fb342c71
Eddie Hsu <aoe102198@gmail.com>
2019-10-25 21:56:52 +0800

    Initial project
    create-react-app by npx

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # heroes-app
 Hahow recruit project, develop repository.
+
+## 1. Environment setup
+npx create-react-app
+
---
+++ src/components/heroes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Heroes extends React.Component {
+    render(){
+       return(
+           <div>
+               <h1> heroesheroesheroesheroes </h1>
+           </div>   
+        )
+     }
+}
