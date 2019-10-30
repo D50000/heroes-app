b0a4a6d022f9548e3cc86bb9f6696f0f2fe8c6a3
Eddie Hsu <aoe102198@gmail.com>
2019-10-30 22:30:45 +0800

    Individual set the url for the image
    For the github server page setup > process.env.NODE_ENV

--- src/components/ProfileContainer.js
+++ src/components/ProfileContainer.js
@@ -3,9 +3,10 @@ import React from 'react';
 export default class ProfileContainer extends React.Component {
 
     render(){
+        const url = process.env.NODE_ENV === "production" ? "/heroes-app" : "";
         return(
             <div className="heroCard batman">
-                <img src="/batman.png" alt="batman" />
+                <img src={`${url}/batman.png`} alt="batman" />
                 <p>Batman: Select a hero above.<span role="img" aria-label="Up Arrow"> ⬆️⬆️⬆️</span></p>
         	</div>
         )
