4d5d6f950459ac0c2987a71061930d8dadb111ed
Eddie Hsu <aoe102198@gmail.com>
2019-10-30 21:31:56 +0800

    change to HashRouter for github server page

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
 import Heroes from './Heroes';
 import ProfileContainer from './ProfileContainer';
