76a1cf96e470cf0cf43ca6e8ec3730f07549c4b2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-11 15:25:21 +0000

    Deploy D50000/heroes-app to github.com/D50000/heroes-app.git:gh-pages

---
+++ precache-manifest.8038b8f72fb2e81a836d033d6c9c5543.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba57dbac59b1c7516150656d76bdc3c",
+    "url": "/heroes-app/index.html"
+  },
+  {
+    "revision": "b7bec46b18ef3b684d82",
+    "url": "/heroes-app/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "3c815dff5cc0cee84eb6",
+    "url": "/heroes-app/static/css/main.d4f1086b.chunk.css"
+  },
+  {
+    "revision": "b7bec46b18ef3b684d82",
+    "url": "/heroes-app/static/js/2.2ffb46b9.chunk.js"
+  },
+  {
+    "revision": "3c815dff5cc0cee84eb6",
+    "url": "/heroes-app/static/js/main.02a1abd0.chunk.js"
+  },
+  {
+    "revision": "e8bf15ef03917c0e6d72",
+    "url": "/heroes-app/static/js/runtime-main.4e3e0927.js"
+  }
+]);
